172abd35fcd7362d1442d3010272cc959a6bb788
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-02-10 15:59:44 +0300

    copmleted assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_reader :match
+
+    def initialize word
+       @word=word
+    end
+
+    def match (possible_anagrams)
+        possible_anagrams.select do |possible_anagram|
+        possible_anagram.downcase.chars.sort == @word.downcase.chars.sort &&
+        possible_anagram.downcase != @word.downcase
+    end
+  end 
+    
+end
